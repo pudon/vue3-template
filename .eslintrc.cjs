0fa244d3b5e6d4e5c32937f7f53d90870b4d79e8
pudon <simple952@163.com>
2024-01-11 16:21:31 +0800

    🌈 style: 临时修改eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
   ],
   overrides: [
     {
@@ -27,5 +27,23 @@ module.exports = {
     'vue',
   ],
   rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      "singleline": {
+        "max": 6,
+      },
+      "multiline": {
+        "max": 1,
+      },
+    }],
   },
 };
