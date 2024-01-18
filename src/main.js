20bfd74e3f051a1cdf3b5966b32412701dc2313d
pudon <simple952@163.com>
2024-01-18 17:26:38 +0800

    🔧 build: 引入pinia持久化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 
 app.mount('#app')
