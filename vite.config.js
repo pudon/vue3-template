993343fc64304e94b0609b153ac1a6dbfd93c964
pudon <simple952@163.com>
2024-01-23 17:31:15 +0800

    🔧 build: 引入element-plus

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import { resolve } from 'path'
 const pathResolve = dir => resolve(__dirname, dir) // 指定解析路径
@@ -12,8 +13,11 @@ export default defineConfig({
     vue(),
     AutoImport({
       imports: ['vue', 'vue-router'],
+      resolvers: [ElementPlusResolver()],
     }),
-    Components(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
