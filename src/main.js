4ccc18c2a59fe145aaaed17644414b45bd1d5b56
pudon <simple952@163.com>
2024-04-13 22:52:12 +0800

    🐳 chore: 引入图标库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
 import '@/assets/styles/index.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
 
+// 全局注册 ElementPlusIconsVue
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
